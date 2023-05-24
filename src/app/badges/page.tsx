3a1174378ef7e551079d8bd475863c0dabda103f
jihwankim255 <plasticat104@gmail.com>
2023-05-25 06:52:55 +0900

    Create badges,bottomNav

---
+++ src/app/badges/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Badges() {
+  return <div>여행 뱃지 페이지</div>;
+}
---
+++ src/components/common/BottomNav.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BottomNav() {
+  return <p>bottom nav here</p>;
+}
